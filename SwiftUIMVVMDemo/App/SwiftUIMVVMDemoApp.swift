072953cb508d4435a647ef6b0f22f01efd04eb94
Valentina <valentina@ValentinadeMacBook-Air.local>
2023-11-12 12:35:57 +0300

    Create a new project to demo MVVM on SwiftUI

---
+++ SwiftUIMVVMDemo/App/SwiftUIMVVMDemoApp.swift
@@ -0,0 +1,14 @@
+//
+//  Created by Valentina Mikheeva on 12.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMVVMDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Hello")
+        }
+    }
+}
